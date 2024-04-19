2b27761b6beb3f9f20a730822e5e6bb328ecbefd
Dmitry Balobanov <dm.balobanov@yandex.ru>
2024-04-20 01:56:58 +0300

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function cards(cardsList) {
+  cardsList.forEach(function(item) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteCardButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    console.log(cardElement);
+    deleteCardButton.addEventListener('click', function() {
+      cardElement.remove();
+    });
+    cardContainer.append(cardElement);
+  });
+}
+
+cards(initialCards);
